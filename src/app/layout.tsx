1dff3d078072ea51b8020701e7f61a2e30459fbd
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 13:38:50 -0300

    feat: Add GlobalStyles to RootLayout for consistent styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
 "use client";
 
+import { GlobalStyles } from "@/styles/global";
 import { theme } from "@/styles/theme";
 import { ThemeProvider } from "styled-components";
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <ThemeProvider theme={theme}>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<html lang="en">
+				<body>{children}</body>
+			</html>
+		</ThemeProvider>
+	);
 }
