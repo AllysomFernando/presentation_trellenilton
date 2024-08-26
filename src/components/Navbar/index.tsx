51d2f676664c15f8e88fbc0fcc1f6465c663ecf4
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 14:40:42 -0300

    feat: Update Navbar component with new links and logo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,26 +1,31 @@
 import { Utility } from "@/styles/utility";
-import { styles } from "./styles";
-
+import { S } from "./styles";
+import Logo from "@/assets/logo.svg";
 export default function NavBar() {
 	return (
-		<styles.Container>
-			<styles.Main>
-				<li>
-					<a href="/">Home</a>
-				</li>
-				<li>
-					<a href="/">Soluções</a>
-				</li>
-				<li>
-					<a href="/">Depoimentos</a>
-				</li>
-				<li>
-					<a href="/">Preços</a>
-				</li>
-				<li>
-					<a href="/">Contato</a>
-				</li>
-			</styles.Main>
-		</styles.Container>
+		<S.Container>
+			<S.Nav>
+				<Utility.DesktopOnly>
+					<S.UlMaster>
+						<img src={Logo} alt="Logo Trellenilton" width={200} height={80} />
+						<li>
+							<a href="/">Home</a>
+						</li>
+						<li>
+							<a href="/">Soluções</a>
+						</li>
+						<li>
+							<a href="/">Depoimentos</a>
+						</li>
+						<li>
+							<a href="/">Preços</a>
+						</li>
+						<li>
+							<a href="/">Contato</a>
+						</li>
+					</S.UlMaster>
+				</Utility.DesktopOnly>
+			</S.Nav>
+		</S.Container>
 	);
 }
