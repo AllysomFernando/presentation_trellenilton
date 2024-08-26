0edc2ab12826eae835cb9e5368deccf96f8c80e9
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 12:39:51 -0300

    chore: Update theme file with new color and font options

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,40 @@
 export const theme = {
     main : {
-        
+        colors: {
+            primary: "#23A6F0",
+            secondart: " #B2E3FF",
+            ligthGray: "#F8F8F8",    
+            ligthTextColor: "#737373",
+            textColor: "#252B42",
+            backgroundColor: "#FFF",
+        },
+        font: {
+            family : {
+                primary: "Montserrat sytem-ui, sans-serif",
+            },
+            weight: {
+                base: 400,
+                md: 500,
+                bold: 800,
+            },
+            size: {
+                sm: "0.875rem",
+                base: "1rem",
+                lg: "1.25rem",
+                xl: "1.75rem",
+                xxl: "2.5rem",
+            }
+        }, 
+        padding: {
+            y:{
+                sm: "0.5rem",
+                base: "1rem",
+                lg: "1.5rem",
+            },
+            x: {
+                lg: "1.5rem",
+                xl: "3rem", 
+            }
+        }
     }
 }
