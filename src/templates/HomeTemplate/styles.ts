a78a63948f727a2371f2976e497a5eca8fc535b1
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 12:31:06 -0300

    feat: Add styles for HomeTemplate component

---
+++ src/templates/HomeTemplate/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const styles = {
+	Container: styled.div`
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	`,
+	Main: styled.div`
+		display: flex;
+		width: 100%;
+	`,
+	Header: styled.header``,
+};
