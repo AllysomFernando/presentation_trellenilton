224825e8dcaf74e924c82b2c1edca150cbdd02ec
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 17:30:29 -0300

    feat: Add close icon to useIcon hook

--- src/hooks/useIcon.tsx
+++ src/hooks/useIcon.tsx
@@ -1,11 +1,12 @@
 import profileSrc from "@/assets/icons/menu-usuario.svg";
 import menuSrc from "@/assets/icons/icon-menu.svg";
-
+import closeSrc from "@/assets/icons/close.svg";
 export type IconsNames = keyof typeof icons;
 
 const icons = {
 	profile: profileSrc,
 	menu: menuSrc,
+	close: closeSrc,
 };
 
 export const useIcon = () => {
