65adbaf3bf3ffb69b77ad1e48335977e04608814
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 15:10:45 -0300

    refactor: Remove SVG exclusion from webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	webpack: (config) => {
-		// Encontre a regra existente para imagens e exclua SVGs dela
-		const fileLoaderRule = config.module.rules.find(
-			(rule) => rule.test && rule.test.test(".svg")
-		);
-		fileLoaderRule.exclude = /\.svg$/;
-
-		// Adicione uma nova regra para processar SVGs usando @svgr/webpack
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: ["@svgr/webpack"],
-		});
-
-		return config;
-	},
-};
+const nextConfig = {};
 
 export default nextConfig;
