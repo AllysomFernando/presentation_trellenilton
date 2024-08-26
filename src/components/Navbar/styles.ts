15e45da6eab0b2ccf1371fde38c35b8c0745354e
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 14:32:09 -0300

    feat: Update Navbar component with new links and styles

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -1,6 +1,18 @@
-import styled from 'styled-components';
+import { theme } from "@/styles/theme";
+import { Utility } from "@/styles/utility";
+import styled from "styled-components";
 
-
-export const Nav = styled.nav`
-    
-`;
+export const styles = {
+	Container: styled.div`
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		--max-width: 680px;
+		width: min(var(--max-width), 100% - var(--px-lg) * 2);
+		margin-inline: auto;
+	`,
+	Main: styled.div`
+		display: flex;
+		gap: ${theme.responsive.gap.base};
+	`,
+};
