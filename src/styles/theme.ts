062f3200e946181837aa18069d5095758473b9ab
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 13:38:47 -0300

    chore: Update theme file with new color and font options

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,40 +1,56 @@
 export const theme = {
-    main : {
-        colors: {
-            primary: "#23A6F0",
-            secondart: " #B2E3FF",
-            ligthGray: "#F8F8F8",    
-            ligthTextColor: "#737373",
-            textColor: "#252B42",
-            backgroundColor: "#FFF",
-        },
-        font: {
-            family : {
-                primary: "Montserrat sytem-ui, sans-serif",
-            },
-            weight: {
-                base: 400,
-                md: 500,
-                bold: 800,
-            },
-            size: {
-                sm: "0.875rem",
-                base: "1rem",
-                lg: "1.25rem",
-                xl: "1.75rem",
-                xxl: "2.5rem",
-            }
-        }, 
-        padding: {
-            y:{
-                sm: "0.5rem",
-                base: "1rem",
-                lg: "1.5rem",
-            },
-            x: {
-                lg: "1.5rem",
-                xl: "3rem", 
-            }
-        }
-    }
-}
+	main: {
+		colors: {
+			primary: "#23A6F0",
+			secondary: " #B2E3FF",
+			ligthGray: "#F8F8F8",
+			ligthTextColor: "#737373",
+			textColor: "#252B42",
+			backgroundColor: "#FFF",
+		},
+		font: {
+			family: {
+				primary: "Montserrat sytem-ui, sans-serif",
+			},
+			weight: {
+				base: 400,
+				md: 500,
+				bold: 800,
+			},
+			size: {
+				sm: "0.875rem",
+				base: "1rem",
+				lg: "1.25rem",
+				xl: "1.75rem",
+				xxl: "2.5rem",
+			},
+		},
+		padding: {
+			y: {
+				sm: "0.5rem",
+				base: "1rem",
+				lg: "1.5rem",
+			},
+			x: {
+				lg: "1.5rem",
+				xl: "3rem",
+			},
+		},
+	},
+	responsive: {
+		fontSize: {
+			lg: "2rem",
+			xl: "2.625rem",
+			xxl: "4rem",
+		},
+		padding: {
+			sm: "1rem",
+			base: "1.5rem",
+			lg: "2.5rem",
+			xl: "5rem",
+		},
+		gap: {
+			lg: "2rem",
+		},
+	},
+};
