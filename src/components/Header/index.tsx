59ffa23160573795d903aa3281a7e90f51503b0b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 14:31:57 -0300

    feat: Add Header component with Navbar

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { Utility } from "@/styles/utility";
+import NavBar from "../Navbar";
+import { styles } from "./styles";
+export default function Header() {
+	return (
+		<styles.Container>
+			<styles.Main>
+				<NavBar />
+			</styles.Main>
+		</styles.Container>
+	);
+}
