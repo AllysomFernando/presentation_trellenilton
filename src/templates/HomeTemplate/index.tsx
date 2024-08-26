5ff908e9b5276078881967b189f3ba0ca224a785
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 12:31:02 -0300

    feat: Add NavBar component to HomeTemplate

--- src/templates/HomeTemplate/index.tsx
+++ src/templates/HomeTemplate/index.tsx
@@ -1,7 +1,13 @@
+import NavBar from "@/components/Navbar";
 import React from "react";
+import { styles } from "./styles";
 
 export default function HomeTemplate() {
 	return (
-		
-	)
+		<styles.Container>
+			<styles.Main>
+				<NavBar />
+			</styles.Main>
+		</styles.Container>
+	);
 }
