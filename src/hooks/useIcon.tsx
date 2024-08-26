b892fdf7601c21468cc9ce4132370497c5f130d2
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 16:58:35 -0300

    feat: Add menu icon to useIcon hook

--- src/hooks/useIcon.tsx
+++ src/hooks/useIcon.tsx
@@ -1,10 +1,12 @@
 import Profile from "@/assets/icons/menu-usuario.svg";
+import Menu from "@/assets/icons/icon-menu.svg";
 import { SVGProps } from "react";
 
 export type IconsNames = keyof typeof icons;
 
 const icons = {
 	profile: (props: SVGProps<SVGSVGElement>) => <Profile {...props} />,
+	menu: (props: SVGProps<SVGSVGElement>) => <Menu {...props} />,
 };
 
 export const useIcon = () => {
