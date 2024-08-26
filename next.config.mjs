6f329975f9ca1d95e8cbc8ddc67e4fc63e02543b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 15:05:31 -0300

    feat: Add SVG support to webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack: (config) => {
+		// Encontre a regra existente para imagens e exclua SVGs dela
+		const fileLoaderRule = config.module.rules.find(
+			(rule) => rule.test && rule.test.test(".svg")
+		);
+		fileLoaderRule.exclude = /\.svg$/;
+
+		// Adicione uma nova regra para processar SVGs usando @svgr/webpack
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ["@svgr/webpack"],
+		});
+
+		return config;
+	},
+};
 
 export default nextConfig;
