3cafdb462ea1839bef33075d26fdd5a59eb63807
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 14:32:05 -0300

    feat: Update Navbar component with new links and styles

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,26 @@
-export default function NavBar () {
+import { Utility } from "@/styles/utility";
+import { styles } from "./styles";
+
+export default function NavBar() {
 	return (
-	  <nav>
-		<ul>
-		  <li>Home</li>
-		  <li>About</li>
-		  <li>Contact</li>
-		</ul>
-	  </nav>
+		<styles.Container>
+			<styles.Main>
+				<li>
+					<a href="/">Home</a>
+				</li>
+				<li>
+					<a href="/">Soluções</a>
+				</li>
+				<li>
+					<a href="/">Depoimentos</a>
+				</li>
+				<li>
+					<a href="/">Preços</a>
+				</li>
+				<li>
+					<a href="/">Contato</a>
+				</li>
+			</styles.Main>
+		</styles.Container>
 	);
-  }
+}
