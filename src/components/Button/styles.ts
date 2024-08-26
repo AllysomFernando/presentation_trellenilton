626c3aea78e4289f945024787cd7ac946c3dc73b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 16:01:15 -0300

    feat: Update Button component styles

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,58 @@
+import { colors } from "@/styles/colors";
+import { theme } from "@/styles/theme";
+import styled from "styled-components";
+
+export const S = {
+	Main: styled.button`
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: ${({ disabled }) =>
+			disabled ? colors.grafiti[600] : colors.purple[600]};
+		color: ${({ disabled }) =>
+			disabled ? colors.grafiti[200] : colors.white[900]};
+		width: 100%;
+		border-radius: 5px;
+		padding: 12px 16px;
+		flex-shrink: 1;
+	`,
+	Outline: styled.button`
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-color: ${({ disabled }) =>
+			disabled ? colors.grafiti[300] : colors.purple[700]};
+		border-width: 2px;
+		color: ${({ disabled }) =>
+			disabled ? colors.grafiti[300] : colors.purple[500]};
+		width: 100%;
+		border-radius: 5px;
+		padding: 12px 16px;
+		flex-shrink: 1;
+		border-style: solid;
+	`,
+	Danger: styled.button`
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: ${colors.red[400]};
+		color: white;
+		width: auto;
+		border-radius: 5px;
+		padding: 12px 16px;
+	`,
+	Text: styled.span`
+		font-size: ${theme.main.font.size.base};
+		color: ${colors.white[900]};
+		font-family: ${theme.main.font.weight.bold};
+	`,
+	Ghost: styled.button`
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: ${colors.purple[200]};
+		width: 100%;
+		padding: 4px;
+		flex-shrink: 1;
+	`,
+};
