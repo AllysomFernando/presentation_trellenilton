43b4d00c41341fec7ea8593d63130e7c54131673
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-08-26 13:56:45 -0300

    chore: Add utility styles for alignment and layout

--- src/styles/utility.ts
+++ src/styles/utility.ts
@@ -16,9 +16,44 @@ export const Utility = {
 	`,
 	DesktopOnly: styled.div`
 		display: none;
+
+		@media ${theme.device.lg} {
+			display: initial;
+		}
 	`,
 	EvenColumns: styled.div`
 		display: grid;
 		gap: ${theme.responsive.gap.base};
+	`,
+	ItemCenter: styled.div`
+		align-items: center;
+	`,
+	JustifyBetween: styled.div`
+		justify-content: space-between;
+	`,
+	Flex: styled.div`
+		display: flex;
+	`,
+	Grid: styled.div`
+		display: grid;
+	`,
+	Gap1: styled.div`
+		gap: ${theme.responsive.gap.base};
+	`,
+	GapOneHalf: styled.div`
+		gap: 1.5rem;
+	`,
+	BtnWrapper: styled.div`
+		cursor: pointer;
+		padding: 2px;
+	`,
+	Container: styled.div`
+		--max-width: 680px;
+		width: min(var(--max-width), 100% - var(--px-lg) * 2);
+		margin-inline: auto;
+
+		@media (width >= 1280px) {
+			--max-width: 1050px;
+		}
 	`,
 };
