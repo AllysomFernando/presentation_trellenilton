5033adfef125ed41328ad6d1bd1c7b19a77e3c62
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-10 11:50:24 -0300

    Refatorar componente Cards para adicionar animação de expansão

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -15,51 +15,53 @@ interface CardListProps {
 
 export default function Cards({ items }: CardListProps) {
 	const [selectedId, setSelectedId] = useState<number | null>(null);
-	
-	return (
-		<>
-			<S.CardContainer>
-				{items.map((item) => (
-					<S.Card
-						layoutId={item.id.toString()}
-						key={item.id}
-						onClick={() => setSelectedId(item.id)}
-					>
-						<S.CardImage
-							src={item.img}
-							alt={item.title}
-							layoutId={`image-${item.id}`}
-						/>
-						<S.CardTitle layoutId={`title-${item.id}`}>
-							{item.title}
-						</S.CardTitle>
-						<S.CardSubtitle layoutId={`subtitle-${item.id}`}>
-							{item.subtitle}
-						</S.CardSubtitle>
-					</S.Card>
-				))}
-			</S.CardContainer>
 
-			<AnimatePresence>
-				{selectedId && (
-					<S.ExpandedCard layoutId={selectedId.toString()}>
-						<S.CardImage
-							src={items.find((item) => item.id === selectedId)?.img}
-							alt={items.find((item) => item.id === selectedId)?.title}
-							layoutId={`image-${selectedId}`}
-						/>
-						<S.CardTitle layoutId={`title-${selectedId}`}>
-							{items.find((item) => item.id === selectedId)?.title}
-						</S.CardTitle>
-						<S.CardSubtitle layoutId={`subtitle-${selectedId}`}>
-							{items.find((item) => item.id === selectedId)?.subtitle}
-						</S.CardSubtitle>
-						<S.CloseButton onClick={() => setSelectedId(null)}>
-							Close
-						</S.CloseButton>
-					</S.ExpandedCard>
-				)}
-			</AnimatePresence>
-		</>
+	return (
+		<S.Container>
+			<S.Main>
+				<S.CardContainer>
+					{items.map((item) => (
+						<S.Card
+							layoutId={item.id.toString()}
+							key={item.id}
+							onClick={() => setSelectedId(item.id)}
+						>
+							<S.CardImage
+								src={item.img}
+								alt={item.title}
+								layoutId={`image-${item.id}`}
+							/>
+							<S.CardTitle layoutId={`title-${item.id}`}>
+								{item.title}
+							</S.CardTitle>
+							<S.CardSubtitle layoutId={`subtitle-${item.id}`}>
+								{item.subtitle}
+							</S.CardSubtitle>
+						</S.Card>
+					))}
+				</S.CardContainer>
+
+				<AnimatePresence>
+					{selectedId && (
+						<S.ExpandedCard layoutId={selectedId.toString()}>
+							<S.CardImage
+								src={items.find((item) => item.id === selectedId)?.img}
+								alt={items.find((item) => item.id === selectedId)?.title}
+								layoutId={`image-${selectedId}`}
+							/>
+							<S.CardTitle layoutId={`title-${selectedId}`}>
+								{items.find((item) => item.id === selectedId)?.title}
+							</S.CardTitle>
+							<S.CardSubtitle layoutId={`subtitle-${selectedId}`}>
+								{items.find((item) => item.id === selectedId)?.subtitle}
+							</S.CardSubtitle>
+							<S.CloseButton onClick={() => setSelectedId(null)}>
+								Close
+							</S.CloseButton>
+						</S.ExpandedCard>
+					)}
+				</AnimatePresence>
+			</S.Main>
+		</S.Container>
 	);
 }
