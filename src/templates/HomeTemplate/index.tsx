cace2c6267891f357f1d59469c00b5c9b219ad04
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-09 21:56:18 -0300

    feat: Adicionar componente Banner à página inicial

--- src/templates/HomeTemplate/index.tsx
+++ src/templates/HomeTemplate/index.tsx
@@ -4,12 +4,14 @@ import React from "react";
 import { S } from "./styles";
 import Header from "@/components/Header";
 import Section from "@/components/Components";
+import Banner from "@/components/Banner";
 
 export default function HomeTemplate() {
 	return (
 		<S.Container>
 			<S.Main>
 				<Header />
+				<Banner />
 				<Section />
 			</S.Main>
 		</S.Container>
