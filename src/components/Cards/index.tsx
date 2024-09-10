e3b5876280838b6f17e4faef4c894af2a918f5dc
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-10 11:24:49 -0300

    feat: Adicionar componente Cards para exibir uma lista de cartões interativos

---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { S } from "./styles";
+import { AnimatePresence } from "framer-motion";
+
+type CardProps = {
+	id: number;
+	title: string;
+	subtitle: string;
+	img: string;
+};
+
+interface CardListProps {
+	items: CardProps[];
+}
+
+export default function Cards({ items }: CardListProps) {
+	const [selectedId, setSelectedId] = useState<number | null>(null);
+	
+	return (
+		<>
+			<S.CardContainer>
+				{items.map((item) => (
+					<S.Card
+						layoutId={item.id.toString()}
+						key={item.id}
+						onClick={() => setSelectedId(item.id)}
+					>
+						<S.CardImage
+							src={item.img}
+							alt={item.title}
+							layoutId={`image-${item.id}`}
+						/>
+						<S.CardTitle layoutId={`title-${item.id}`}>
+							{item.title}
+						</S.CardTitle>
+						<S.CardSubtitle layoutId={`subtitle-${item.id}`}>
+							{item.subtitle}
+						</S.CardSubtitle>
+					</S.Card>
+				))}
+			</S.CardContainer>
+
+			<AnimatePresence>
+				{selectedId && (
+					<S.ExpandedCard layoutId={selectedId.toString()}>
+						<S.CardImage
+							src={items.find((item) => item.id === selectedId)?.img}
+							alt={items.find((item) => item.id === selectedId)?.title}
+							layoutId={`image-${selectedId}`}
+						/>
+						<S.CardTitle layoutId={`title-${selectedId}`}>
+							{items.find((item) => item.id === selectedId)?.title}
+						</S.CardTitle>
+						<S.CardSubtitle layoutId={`subtitle-${selectedId}`}>
+							{items.find((item) => item.id === selectedId)?.subtitle}
+						</S.CardSubtitle>
+						<S.CloseButton onClick={() => setSelectedId(null)}>
+							Close
+						</S.CloseButton>
+					</S.ExpandedCard>
+				)}
+			</AnimatePresence>
+		</>
+	);
+}
