7a40e637cdb4b66ecf2f79329f54f2c8e58a1b20
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-10 10:48:34 -0300

    feat: Atualizar estilos do componente Main na página inicial

--- src/components/Components/styles.ts
+++ src/components/Components/styles.ts
@@ -8,30 +8,52 @@ export const S = {
 		display: flex;
 		flex-direction: row;
 		justify-content: center;
-        gap: 15rem;
-        align-items: center;
-        max-width: 1440px;
+		gap: 15rem;
+		align-items: center;
+		padding: 2rem;
+		text-align: center;
+		@media (min-width: 768px) {
+			flex-direction: row;
+			justify-content: space-between;
+			padding: 4rem;
+			text-align: left;
+		}
 	`,
 	Main: styled.div`
 		width: 100%;
 		padding: 0 ${theme.main.spacing.x.xl};
 	`,
-	ButtonConatiners: styled.div`
+	ButtonContainers: styled.div`
 		display: flex;
-		justify-content: center;
-		gap: 10rem;
+		flex-direction: column;
+		gap: 1rem;
+		margin-top: 2rem;
+
+		@media (min-width: 768px) {
+			flex-direction: row;
+			justify-content: center;
+			gap: 2rem;
+		}
 	`,
 	Image: styled(Image)`
-        width: 1200px;
-        height: 100%;    
-        border-radius: 5%;
-    `,
+		width:  350px;
+		height: 100%;
+		border-radius: 5%;
+
+		@media (max-width: 768px) {
+			height: 300px;
+			width: 300px;
+			margin: 0 auto;
+		}
+	`,
 	ImageContainer: styled.div`
 		display: flex;
 		justify-content: center;
 		align-items: center;
-        padding: 5rem 5rem;
-		width: 30%;
-		height: 30%;
+		width: 100%;
+
+		@media (min-width: 768px) {
+			width: 40%;
+		}
 	`,
 };
