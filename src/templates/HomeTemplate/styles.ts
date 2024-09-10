5528f2a8b6a5b4b3007d19319620a353ae909b88
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-10 11:25:04 -0300

    Atualizar estilos do componente Main na página inicial

--- src/templates/HomeTemplate/styles.ts
+++ src/templates/HomeTemplate/styles.ts
@@ -3,15 +3,20 @@ import styled from "styled-components";
 
 export const S = {
 	Container: styled.div`
+		max-width: 1440px;
 		display: flex;
 		flex-direction: row;
+		align-items: center;
 		justify-content: center;
 	`,
 	Main: styled.div`
 		width: 100%;
-		max-width: 1440px;
 		padding: 0 ${theme.main.spacing.x.xl};
-		
+		align-items: center;
+
+		/* justify-content: center;
+		display: flex;
+		flex-direction: column; */
 	`,
 	Header: styled.header``,
 };
