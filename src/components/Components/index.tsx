0272374afbe5d53cf95294495e5c7d291cf3be61
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-09 21:56:01 -0300

    feat: Adicionar estilos animados ao componente Banner

--- src/components/Components/index.tsx
+++ src/components/Components/index.tsx
@@ -1,31 +1,24 @@
 import { Utility } from "@/styles/utility";
-import Image from "next/image";
-import HeroRectangleOne from "@/assets/images/rectangleOne.png";
-import HeroRectangleTwo from "@/assets/images/rectangleTwo.png";
 import { Button } from "../Button";
+import { S } from "./styles";
+import DefaultHomeImage from "@/assets/images/home.jpeg";
+
 export default function Section() {
 	const handleCadastro = () => {
 		console.log("cadastro");
 	};
 	return (
 		<section>
-			<Utility.DesktopOnlySpan>
-				<Utility.Image src={HeroRectangleOne} alt="Rectangle One" />
-			</Utility.DesktopOnlySpan>
-			<Utility.Image src={HeroRectangleTwo} alt="Rectangle One" />
-			<Utility.ContainerContent>
+			<S.Container>
 				<Utility.DesktopOnly>
-					<Utility.DesktopOnlyP>Olá</Utility.DesktopOnlyP>
-					<h1>
-						Somos um kanban de open source, ajudando a produtividade dos
-						engenheiros de softwares
-					</h1>
+					<h1>Quem somos?</h1>
 					<p>
 						Somos uma equipe de desenvolvedores que enfrantavamos porblemas em
 						organizar as tarefas do dia dia, e os softwares de hoje não estava
-						de agrado nosso
+						de agrado nosso. Então decidimos criar o nosso próprio software de
+						organização de tarefas.
 					</p>
-					<Utility.FlexGap>
+					<S.ButtonConatiners>
 						<Button title="Começar" variant="Main" onPress={handleCadastro} />
 						<Utility.DesktopOnlySpan>
 							<Button
@@ -34,9 +27,12 @@ export default function Section() {
 								onPress={handleCadastro}
 							/>
 						</Utility.DesktopOnlySpan>
-					</Utility.FlexGap>
+					</S.ButtonConatiners>
 				</Utility.DesktopOnly>
-			</Utility.ContainerContent>
+				<S.ImageContainer>
+					<S.Image src={DefaultHomeImage} alt="Imagem para acompanhar" />
+				</S.ImageContainer>
+			</S.Container>
 		</section>
 	);
 }
