2ef33962176f2b167f0e64abdc65f1d384d34cd3
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-11 09:54:35 -0300

    Refatorar estilo do menu mobile na Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,11 +4,33 @@ import Logo from "@/assets/logo.svg";
 import { Button } from "@/components/Button";
 import { useState } from "react";
 import { Icon } from "@/components/Icon";
+import { motion } from "framer-motion";
+
 export default function NavBar() {
 	const [mobile, setMobile] = useState(false);
 	const handlePress = () => {
 		console.log("Button pressed");
 	};
+
+	const menuVariants = {
+		open: {
+			opacity: 1,
+			x: 0,
+			transition: {
+				duration: 0.5,
+				ease: "easeInOut",
+			},
+		},
+		closed: {
+			opacity: 0,
+			x: "-100%",
+			transition: {
+				duration: 0.5,
+				ease: "easeInOut",
+			},
+		},
+	};
+
 	return (
 		<S.Container>
 			<S.Nav>
@@ -37,51 +59,62 @@ export default function NavBar() {
 					<Button onPress={handlePress} title="Cadastrar" variant="Main" />
 				</S.ButtonContainer>
 			</S.Nav>
+
 			<S.MenuMobileContainer>
-				{mobile ? (
-					<S.MenuMobileContainer>
-						<S.MenuMobileContent>
-							<Utility.ContainerMobile>
-								<ul>
-									<li>
-										<a href="#">Home</a>
-									</li>
-									<li>
-										<a href="#solution">Soluções</a>
-									</li>
-									<li>
-										<a href="#testimonials">Depoimentos</a>
-									</li>
-									<li>
-										<a href="#pricing">Preços</a>
-									</li>
-									<li>
-										<a href="#contact">Contato</a>
-									</li>
-								</ul>
-								<Utility.BtnWrapper onClick={() => setMobile(!mobile)}>
-									<Icon
-										name="close"
-										width={35}
-										height={35}
-										style={{
-											position: "absolute",
-											top: "2.3rem",
-											right: "5rem",
-										}}
-									/>
-								</Utility.BtnWrapper>
-							</Utility.ContainerMobile>
-						</S.MenuMobileContent>
-					</S.MenuMobileContainer>
-				) : (
-					<S.MenuContainerLogo>
-						<Utility.Image src={Logo} alt="Logo Trellenilton" width={200} />
-						<Utility.BtnWrapper onClick={() => setMobile(!mobile)}>
-							<Icon name="menu" width={35} height={35} />
-						</Utility.BtnWrapper>
-					</S.MenuContainerLogo>
-				)}
+				<S.MenuContainerLogo>
+					<Utility.Image src={Logo} alt="Logo Trellenilton" width={200} />
+					<Utility.BtnWrapper onClick={() => setMobile(!mobile)}>
+						<Icon name="menu" width={35} height={35} />
+					</Utility.BtnWrapper>
+				</S.MenuContainerLogo>
+
+				<motion.div
+					initial={false}
+					animate={mobile ? "open" : "closed"}
+					variants={menuVariants}
+					style={{
+						position: "absolute",
+						top: 0,
+						left: 0,
+						width: "100%",
+						height: "100vh",
+						backgroundColor: "white",
+					}}
+				>
+					<S.MenuMobileContent>
+						<Utility.ContainerMobile>
+							<ul>
+								<li>
+									<a href="#">Home</a>
+								</li>
+								<li>
+									<a href="#solution">Soluções</a>
+								</li>
+								<li>
+									<a href="#testimonials">Depoimentos</a>
+								</li>
+								<li>
+									<a href="#pricing">Preços</a>
+								</li>
+								<li>
+									<a href="#contact">Contato</a>
+								</li>
+							</ul>
+							<Utility.BtnWrapper onClick={() => setMobile(!mobile)}>
+								<Icon
+									name="close"
+									width={35}
+									height={35}
+									style={{
+										position: "absolute",
+										top: "2.3rem",
+										right: "5rem",
+									}}
+								/>
+							</Utility.BtnWrapper>
+						</Utility.ContainerMobile>
+					</S.MenuMobileContent>
+				</motion.div>
 			</S.MenuMobileContainer>
 		</S.Container>
 	);
