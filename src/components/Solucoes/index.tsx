5e47a96ba3569c3030127a72bebf3bd102f963a3
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-11 09:39:50 -0300

    Refatorar estilo do título na página de Soluções

--- src/components/Solucoes/index.tsx
+++ src/components/Solucoes/index.tsx
@@ -1,13 +1,14 @@
 import Cards from "../Cards";
 import { TextCards } from "@/components/Cards/text";
 import { S } from "./styles";
+import { Utility } from "@/styles/utility";
 
 export function Solucoes() {
 	return (
 		<S.Container>
 			<S.Main>
 				<S.Content>
-					<h1>Soluções</h1>
+					<S.H1>Soluções</S.H1>
 					<Cards items={TextCards} />
 				</S.Content>
 			</S.Main>
