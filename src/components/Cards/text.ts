9cec42d964aa0c80ba5969a51c4d6b544f7f3900
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-09-11 09:24:06 -0300

    Refatorar importação de imagens nos cartões de Cards

--- src/components/Cards/text.ts
+++ src/components/Cards/text.ts
@@ -1,24 +1,26 @@
-
+import ImageAgility from "@/assets/images/agilidade.jpeg";
+import ImageOrganization from "@/assets/images/organizacao.jpeg";
+import ImageTransparency from "@/assets/images/transparencia.jpeg";
 export const TextCards = [
 	{
 		id: 1,
 		title: "Agilidade",
 		subtitle: "Agilidade no dia a dia",
 		text: "Aqui você encontra soluções para otimizar o seu tempo e aumentar a sua produtividade.",
-		img: "https://via.placeholder.com/400x200",
+		img: ImageAgility,
 	},
 	{
 		id: 2,
 		title: "Organização",
 		subtitle: "Organização é a chave",
 		text: "Aqui você encontra soluções para otimizar o seu tempo e aumentar a sua produtividade.",
-		img: "https://via.placeholder.com/400x200",
+		img: ImageOrganization,
 	},
 	{
 		id: 3,
 		title: "Transparência",
 		subtitle: "Transparência é tudo",
 		text: "Na Trellenilton você encontra soluções para otimizar o seu tempo e aumentar a sua produtividade.",
-		img: "https://via.placeholder.com/400x200",
+		img: ImageTransparency,
 	},
 ];
