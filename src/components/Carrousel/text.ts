b769dd5e9ba55516ece552c582e5317c94855b06
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-11-04 20:18:52 -0300

    feat: add client testimonials data for Carrousel component

---
+++ src/components/Carrousel/text.ts
@@ -0,0 +1,35 @@
+import ClientImage1 from "@/assets/images/Figma Image 400x492.png";
+import ClientImage2 from "@/assets/images/Figma Image 428x498.png";
+import ClientImage3 from "@/assets/images/Figma Image 400x492.png";
+import ClientImage4 from "@/assets/images/Figma Image 428x498.png";
+
+export const Clients = [
+	{
+		id: 1,
+		title: "Cliente 1",
+		subtitle: "Satisfação garantida",
+		text: "Cliente 1 está muito satisfeito com nossos serviços.",
+		img: ClientImage1,
+	},
+	{
+		id: 2,
+		title: "Cliente 2",
+		subtitle: "Qualidade e eficiência",
+		text: "Cliente 2 destaca a qualidade e eficiência de nossos produtos.",
+		img: ClientImage2,
+	},
+	{
+		id: 3,
+		title: "Cliente 3",
+		subtitle: "Inovação constante",
+		text: "Cliente 3 aprecia nossa constante inovação.",
+		img: ClientImage3,
+	},
+	{
+		id: 4,
+		title: "Cliente 4",
+		subtitle: "Parceria de sucesso",
+		text: "Cliente 4 valoriza nossa parceria de sucesso.",
+		img: ClientImage4,
+	},
+];
