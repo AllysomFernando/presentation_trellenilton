305d766456ff11f78d5bb84d5882bd284735b347
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-11-06 09:10:35 -0300

    refactor: enhance Cliente component structure with styled components

--- src/components/Cliente/index.tsx
+++ src/components/Cliente/index.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Carrousel from "../Carrousel";
 import { Clients } from "../Carrousel/text";
+import { S } from "./styles";
 
 export default function Cliente() {
 	return (
-		<div>
-			<h3>Conselho de quem conhece</h3>
-			<h1>Cada cliente importa!</h1>
-			<p>
-				Problems trying to resolve the conflict between the two major realms of
-				Classical physics: Newtonian mechanics{" "}
-			</p>
-			<Carrousel items={Clients} />
-		</div>
+		<S.Container>
+			<S.Main>
+				<h3>Conselho de quem conhece</h3>
+				<h1>Cada cliente importa!</h1>
+				<S.Paragraph>
+					Problems trying to resolve the conflict between the two major realms
+					of Classical physics: Newtonian mechanics{" "}
+				</S.Paragraph>
+				<Carrousel items={Clients} />
+			</S.Main>
+		</S.Container>
 	);
 }
