0543f96b35447b8cb4fbb332d7fbf164db2e50b6
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-11-06 14:05:30 -0300

    feat: add optional description to CardPreco component for enhanced pricing details

--- src/components/CardPreco/index.tsx
+++ src/components/CardPreco/index.tsx
@@ -4,6 +4,7 @@ interface CardPrecoProps {
 	title: string;
 	description: string;
 	price: string;
+	precoDescricao?: string;
 	isHighlighted?: boolean;
 }
 
@@ -11,13 +12,20 @@ export default function CardPreco({
 	title,
 	description,
 	price,
+	precoDescricao,
 	isHighlighted,
 }: CardPrecoProps) {
 	return (
 		<S.Plan isHighlighted={isHighlighted}>
 			<S.PlanTitle>{title}</S.PlanTitle>
 			<S.PlanDescription>{description}</S.PlanDescription>
-			<S.PlanPrice>{price}</S.PlanPrice>
+			<S.PriceDiv>
+				<S.PlanPrice>{price}</S.PlanPrice>
+				<S.ParagraphDescriptionPrice>
+					{precoDescricao}
+				</S.ParagraphDescriptionPrice>
+			</S.PriceDiv>
+			<S.HR />
 			<S.PlanButton>Contratar</S.PlanButton>
 		</S.Plan>
 	);
