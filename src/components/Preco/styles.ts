2fa571a535d8d8c600e1367b17913a70160b1181
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-11-06 09:42:14 -0300

    feat: add styled components for Preco styles to enhance layout and responsiveness

---
+++ src/components/Preco/styles.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { theme } from "@/styles/theme";
+
+export const S = {
+	Container: styled.div`
+		display: flex;
+		justify-content: center;
+		gap: 15rem;
+		align-items: center;
+		text-align: center;
+		padding: 5rem 0;
+		@media (max-width: 768px) {
+			flex-direction: row;
+			justify-content: center;
+			padding-top: 5rem;
+			text-align: center;
+		}
+		
+	`,
+	Main: styled.div`
+		width: 100%;
+		padding: 0 ${theme.main.spacing.x.xl};
+		align-items: center;
+		justify-content: center;
+	`,
+	Content: styled.div`
+		text-align: left;
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+	`,
+    Paragraph: styled.p`
+        padding-bottom: 3rem;
+    `
+};
