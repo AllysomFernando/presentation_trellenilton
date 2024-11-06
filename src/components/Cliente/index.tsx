8dd8054b24a9423a6eec950568ba7d367692ff2e
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-11-06 10:21:18 -0300

    feat: wrap headings and paragraph in S.Text component for improved structure in Cliente

--- src/components/Cliente/index.tsx
+++ src/components/Cliente/index.tsx
@@ -7,12 +7,14 @@ export default function Cliente() {
 	return (
 		<S.Container>
 			<S.Main>
-				<S.H3>Conselho de quem conhece</S.H3>
-				<S.H1>Cada cliente importa!</S.H1>
-				<S.Paragraph>
-					Problems trying to resolve the conflict between the two major realms
-					of Classical physics: Newtonian mechanics{" "}
-				</S.Paragraph>
+				<S.Text>
+					<S.H3>Conselho de quem conhece</S.H3>
+					<S.H1>Cada cliente importa!</S.H1>
+					<S.Paragraph>
+						Problems trying to resolve the conflict between the two major realms
+						of Classical physics: Newtonian mechanics{" "}
+					</S.Paragraph>
+				</S.Text>
 				<Carrousel items={Clients} />
 			</S.Main>
 		</S.Container>
