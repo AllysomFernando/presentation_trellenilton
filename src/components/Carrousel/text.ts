2bbc4f37d061edf094aff203b7bc408c7d012a21
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-11-06 09:33:39 -0300

    feat: update client IDs and testimonials in Carrousel component

--- src/components/Carrousel/text.ts
+++ src/components/Carrousel/text.ts
@@ -5,28 +5,28 @@ import ClientImage4 from "@/assets/images/Figma Image 428x498.png";
 
 export const Clients = [
 	{
-		id: 1,
+		id: 8,
 		title: "Rian Gosling",
 		subtitle: "Satisfação garantida",
 		text: "Rian Gosling está muito satisfeito com nossos serviços.",
 		img: ClientImage1,
 	},
 	{
-		id: 2,
+		id: 9,
 		title: "Shellon Musk",
 		subtitle: "Qualidade e eficiência",
 		text: "Shellon Musk destaca a qualidade e eficiência de nossos produtos.",
 		img: ClientImage2,
 	},
 	{
-		id: 3,
+		id: 10,
 		title: "Brendon Urie",
 		subtitle: "Inovação constante",
 		text: "Brendon Urie aprecia nossa constante inovação.",
 		img: ClientImage3,
 	},
 	{
-		id: 4,
+		id: 11,
 		title: "Xi Jinping",
 		subtitle: "Parceria de sucesso",
 		text: "Xi Jinping valoriza nossa parceria de sucesso.",
