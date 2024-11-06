35d22c3423bbcecf57cf0397b02b5aeef8bf6d2a
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-11-06 09:45:34 -0300

    feat: update headings in Preco component for improved clarity and presentation

--- src/components/Preco/index.tsx
+++ src/components/Preco/index.tsx
@@ -4,7 +4,8 @@ export default function Preco() {
 	return (
 		<S.Container>
 			<S.Main>
-				<h1>Preço</h1>
+				<h3>Planos e preços</h3>
+				<h1>Nossos planos</h1>
 			</S.Main>
 		</S.Container>
 	);
