c591b44d1a2c334c6b9e496c21b5eed7bd02fe24
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-11-17 23:20:34 -0300

    feat: enhance Carrousel component with responsive breakpoints and styled container

--- src/components/Carrousel/index.tsx
+++ src/components/Carrousel/index.tsx
@@ -3,28 +3,44 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-
 import Cards, { CardProps } from "../Cards";
+import styled from "styled-components";
 
 type CarrouselProps = {
 	items: CardProps[];
 };
+const SwiperContainer = styled.div`
+	width: 100%;
+	padding: 0 1rem;
+`;
 
 export default function Carrousel({ items }: CarrouselProps) {
 	return (
-		<div>
+		<SwiperContainer>
 			<Swiper
-				spaceBetween={-100}
+				spaceBetween={20}
 				autoplay={{
 					delay: 2500,
 					disableOnInteraction: false,
 				}}
 				pagination={{ clickable: true }}
 				navigation
-				slidesPerView={3}
 				modules={[Pagination, Navigation, Autoplay]}
+				breakpoints={{
+					320: {
+						slidesPerView: 1,
+						spaceBetween: 10,
+					},
+					768: {
+						slidesPerView: 2,
+						spaceBetween: 20,
+					},
+					1200: {
+						slidesPerView: 3,
+						spaceBetween: 30,
+					},
+				}}
 			>
 				{items.map((item) => (
 					<SwiperSlide key={item.id}>
@@ -32,6 +48,6 @@ export default function Carrousel({ items }: CarrouselProps) {
 					</SwiperSlide>
 				))}
 			</Swiper>
-		</div>
+		</SwiperContainer>
 	);
 }
