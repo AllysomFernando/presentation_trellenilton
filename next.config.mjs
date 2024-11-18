3a76ad3c82eb423c8d5071f77d51c27ae89242fe
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-11-18 13:54:13 -0300

    feat: add rewrites for API requests to external cloud function

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination:
+					"https://us-central1-plucky-cascade-440013-j0.cloudfunctions.net/:path*"
+			},
+		];
+	},
+};
 
 export default nextConfig;
